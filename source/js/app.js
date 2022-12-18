76c6d9a780cc5986de67f8a641944b3c316098c6
zws <nooldey@gmail.com>
2022-12-18 18:09:32 +0800

    feat: 删除无用的阅读数、第三方评论，改设置

--- source/js/app.js
+++ source/js/app.js
@@ -1,17 +1,3 @@
-!function checkDomain() {
-    var allow = [
-        'zhuweisheng.com.cn',
-        'nooldey.github.io'
-    ];
-    var u = allow.some(url => {
-        var s = new RegExp(url);
-        return s.test(location.origin)
-    });
-    if (!u) {
-        location.href = location.href.replace(location.host, '//zhuweisheng.com.cn/')
-    }
-}();
-
 /* 识别客户端 */ 
 !function() {
     var ua = navigator.userAgent;
